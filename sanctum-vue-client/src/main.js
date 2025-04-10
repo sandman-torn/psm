213037803d532229bbc74373e1e591ec44911964
Mohammed Satippiddin Safid Hamzah <satippiddin@johor.gov.my>
2025-04-11 00:59:58 +0800

    initial commit

---
+++ sanctum-vue-client/src/main.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import useAuth from './composable/useAuth';
+
+axios.defaults.baseURL = 'http://sanctum-vue-api.test';
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+
+const app = createApp(App);
+const { attempt } = useAuth();
+
+app.use(router);
+
+attempt().then(() => {
+  app.mount('#app');
+});
---
+++ sanctum-vue-client/src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { reactive } from 'vue';
+import useAuth from '@/composable/useAuth';
+import Navigation from '@/components/Navigation.vue';
+
+const { login: loginAction, errors } = useAuth();
+
+
+const form = reactive({
+  email: 'admin@admin.com',
+  password: 'password'
+});
+
+const login = async () => {
+  loginAction(form)
+}
+</script>
+
+<template>
+  <main>
+    <navigation />
+    <form action="" method="post" @submit.prevent="login">
+      <input v-model="form.email" type="text" name="email:" id="email" placeholder="Email" />
+      <p v-if="errors.email">{{ errors.email[0] }}</p>
+      <input v-model="form.password" type="password" name="password:" id="password" placeholder="Password" />
+      <p v-if="errors.password">{{ errors.password[0] }}</p>
+
+      <button type="submit">Login</button>
+    </form>
+  </main>
+</template>
